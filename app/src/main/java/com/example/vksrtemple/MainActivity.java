6f6457fe9d2dcb543dabeb629e6dbd64817be5af
abose <tellarunbose@gmail.com>
2021-04-18 16:06:42 +0530

    Init android web view from https://www.sreerajarajeswaridevi.org/android_app/

--- app/src/main/java/com/example/vksrtemple/MainActivity.java
+++ app/src/main/java/com/example/vksrtemple/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.vksrtemple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.sreerajarajeswaridevi.org/android_app/");
     }
 }
