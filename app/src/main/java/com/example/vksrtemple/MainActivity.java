0e482f76dfcb4c1b4a3ec6d0c5e471531bb78931
abose <tellarunbose@gmail.com>
2021-04-18 19:07:17 +0530

    Persist webview state across orientation and other changes.

--- app/src/main/java/com/example/vksrtemple/MainActivity.java
+++ app/src/main/java/com/example/vksrtemple/MainActivity.java
@@ -8,16 +8,34 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    WebView web;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = (WebView) findViewById(R.id.webview);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new WebAppBridge(this), "Android");
-        webView.loadUrl("https://www.sreerajarajeswaridevi.org/android_app/");
+        web = (WebView) findViewById(R.id.webview);
+        if (savedInstanceState == null) {
+            web.loadUrl("https://www.sreerajarajeswaridevi.org/android_app/");
+            WebSettings webSettings = web.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            web.addJavascriptInterface(new WebAppBridge(this), "Android");
+        } else {
+            web.restoreState(savedInstanceState);
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        web.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        web.restoreState(savedInstanceState);
     }
 }
