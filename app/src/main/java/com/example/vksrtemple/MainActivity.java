4d9fbca3ba0e5b0abc35bbf4eb713fd7f6dc7a4e
abose <tellarunbose@gmail.com>
2021-05-01 12:25:19 +0530

    Enabling html5

--- app/src/main/java/com/example/vksrtemple/MainActivity.java
+++ app/src/main/java/com/example/vksrtemple/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.vksrtemple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
     WebView web;
+    static String TAG = "webview";
+
+    protected void enableWebviewDebug(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))
+            { WebView.setWebContentsDebuggingEnabled(true); }
+        }
+    }
 
     protected void setFullScreen(){
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
@@ -19,18 +33,57 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
     }
 
+    protected void configureWebview(WebView web){
+        web.loadUrl("https://www.sreerajarajeswaridevi.org/android_app/");
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.ECLAIR) {
+            try {
+                Log.d(TAG, "Enabling HTML5-Features");
+                Method m1 = WebSettings.class.getMethod("setDomStorageEnabled", new Class[]{Boolean.TYPE});
+                m1.invoke(webSettings, Boolean.TRUE);
+
+                Method m2 = WebSettings.class.getMethod("setDatabaseEnabled", new Class[]{Boolean.TYPE});
+                m2.invoke(webSettings, Boolean.TRUE);
+
+                Method m3 = WebSettings.class.getMethod("setDatabasePath", new Class[]{String.class});
+                m3.invoke(webSettings, "/data/data/" + getPackageName() + "/databases/");
+
+                Method m4 = WebSettings.class.getMethod("setAppCacheMaxSize", new Class[]{Long.TYPE});
+                m4.invoke(webSettings, 1024*1024*128);
+
+                Method m5 = WebSettings.class.getMethod("setAppCachePath", new Class[]{String.class});
+                m5.invoke(webSettings, "/data/data/" + getPackageName() + "/cache/");
+
+                Method m6 = WebSettings.class.getMethod("setAppCacheEnabled", new Class[]{Boolean.TYPE});
+                m6.invoke(webSettings, Boolean.TRUE);
+
+                Log.d(TAG, "Enabled HTML5-Features");
+            }
+            catch (NoSuchMethodException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+            catch (InvocationTargetException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+            catch (IllegalAccessException e) {
+                Log.e(TAG, "Reflection fail", e);
+            }
+        }
+        web.addJavascriptInterface(new WebAppBridge(this), "Android");
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setFullScreen();
+        enableWebviewDebug();
         setContentView(R.layout.activity_main);
         web = (WebView) findViewById(R.id.webview);
         if (savedInstanceState == null) {
-            web.loadUrl("https://www.sreerajarajeswaridevi.org/android_app/");
-            WebSettings webSettings = web.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            web.addJavascriptInterface(new WebAppBridge(this), "Android");
+            configureWebview(web);
         } else {
             web.restoreState(savedInstanceState);
         }
